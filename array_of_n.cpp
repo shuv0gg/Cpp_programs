85bd3646f9edf52aa885399dcfa90b42b9f2c48e
SHUVOgg <mek234778@gmail.comgit config --global user.email you@example.com>
2024-12-01 21:49:45 +0600

    Array of N elements

---
+++ array_of_n.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int n;
+   cout<<"Enter how many elements : ";
+   cin>>n;
+   int arr[n];
+   for(int i=0;i<n;i++)
+   {
+    cin>>arr[i];
+   }
+   cout<<"The elements are : ";
+   for(int i=0;i<n;i++)
+   {
+    cout<<arr[i]<<" ";
+   }
+
+}
---
+++ sum.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int a,b;
+  cout<<"Enter the values of a & b:  ";
+  cin>>a>>b;
+  cout<<a+b;
+
+
+}
