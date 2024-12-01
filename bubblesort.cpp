87828d553bd26bd4ebb41d60ae28cdcd969085ba
MEK Shuvo <163111769+shuv0gg@users.noreply.github.com>
2024-12-01 22:46:14 +0600

    Bubble Sort

---
+++ bubblesort.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+
+        for (int j = 0; j < n - 1 - i; j++) {
+
+            if (arr[j] > arr[j + 1]) {
+
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int n;
+
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+
+    bubbleSort(arr, n);
+
+
+    cout << "Sorted array: ";
+    printArray(arr, n);
+
+    return 0;
+}
