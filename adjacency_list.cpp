30a8a7d82222da1c2ddcf3a08a175ee4b8269dfe
Shuvo <163111769+shuv0gg@users.noreply.github.com>
2025-08-08 22:43:02 +0600

    Update adjacency_list.cpp

--- adjacency_list.cpp
+++ adjacency_list.cpp
@@ -1,1 +1,29 @@
 
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    vector<int>v[n+1];
+    while(e--)
+    {
+        int x, y;
+        cin >> x >> y;
+        v[x].push_back(y); // x -> y
+        v[y].push_back(x); // y -> x (For Undirected)
+    }
+
+    for(int i=1; i<=n; i++)
+    {
+        cout << i << " => ";
+        int len = (int)v[i].size();
+        for(int j=0; j<len; j++)
+        {
+            cout << v[i][j] << ' ';
+        }
+        cout << endl;
+    }
+}
